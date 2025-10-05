3fb3ac94e0079344a1bedb0d16f4fe4f29981cf6
hanan <hananfijananto@gmail.com>
2025-10-05 20:01:52 +0700

    hover-info-footer

--- src/components/Footer.tsx
+++ src/components/Footer.tsx
@@ -1,4 +1,4 @@
-import { Github, Linkedin, Mail, Heart } from "lucide-react";
+import { Github, Linkedin, Mail, Heart, ArrowDown } from "lucide-react";
 import { TextHoverEffect } from "@/components/ui/text-hover-effect";
 
 const Footer = () => {
@@ -82,9 +82,15 @@ const Footer = () => {
 
         <div className="border-t border-border/50 pt-8 mt-4 flex flex-col md:flex-row justify-between items-center">
           <p className="text-muted-foreground text-sm">©{currentYear} Hanan Fijananto. All rights reserved.</p>
+          <div className="flex flex-row items-center gap-2">
+            <p className="text-muted-foreground text-sm">Hover This</p>
+            <ArrowDown className="h-6 w-6 text-muted-foreground" />
+          </div>
           <p className="text-muted-foreground text-sm flex items-center mt-2 md:mt-0">Made using React + Vite & TypeScript</p>
         </div>
       </div>
+      {/* <div className="absolute bottom-8 left-1/2 transform -translate-x-1/2 animate-bounce"> */}
+
       <div className="hidden sm:block h-[30rem] mt-[-2rem] items-center justify-center ">
         <TextHoverEffect text="HANAN" />
       </div>
